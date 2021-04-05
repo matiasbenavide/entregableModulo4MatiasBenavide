d737b51a87460037905ac049a1d9421a4a04c566
matiasbenavide <matiasbenavide@gmail.com>
2021-04-05 12:46:33 -0300

    Termino el proyecto

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.get("/",(req,res)=>{
 app.get("/register",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/register.html"))
 })
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
 app.listen(port,()=>{
     console.log("Servidor corriendo en el puerto: "+port)
 })
